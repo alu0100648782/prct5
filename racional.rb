9df1e03f61472659123706019867658a2f97b86c
rafael abadia <alu0100648782@ull.edu.es>
2015-10-28 18:34:25 +0000

    parametros de la clase fraccion

---
+++ racional.rb
@@ -0,0 +1,30 @@
+#implementar en este fichero la clase para crear los objetos
+
+require "./gcd.rb"
+
+class Fraccion
+	attr_accessor :n, :d
+	
+	def initialize(n,d)
+      raise RuntimeError unless n.is_a? Integer and d.is_a? Integer
+		@n, @d = n, d
+	end
+
+   def to_s
+      "#{@n}/#{@d}"
+   end
+
+   def reducir(n,d)
+   	mcd = gcd(n,d)
+   	Fraccion.new(n/mcd, d/mcd)
+   end
+
+   def sumayresta(other,f1,f2)
+   	if f1.d != f2.d then
+   		mcm = (f1.d * f2.d) / gcd(f1.d,f2.d)
+   		f1.n = mcm / f1.d * f1.n
+   		f1.d = mcm
+   		f2.n = mcm / f2.d * f2.n
+   		f2.d = mcm
+   	end
+   end
